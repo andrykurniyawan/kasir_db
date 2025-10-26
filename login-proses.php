fd2e6572a7bcd162b90364127a6abc7ed350fe91
ANDRY KURNIYAWAN <andrekurniawan3041@gmail.com>
2025-10-26 19:37:03 +0700

    Update login-proses.php

--- login-proses.php
+++ login-proses.php
@@ -1,5 +1,5 @@
 <?php
-    if ($_POST['username'] == "admin" && $_POST['password'] == "123") {
+    if ($_POST['username'] == "admin" && $_POST['password'] == "1234") {
         //echo "Login Berhasil";
         session_start();
         $_SESSION['username'] = $_POST['username'] ;
